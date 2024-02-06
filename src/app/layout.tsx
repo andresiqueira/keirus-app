471d566b92db664ab673c56e2e736112c2425f76
andresiqueira <andresiqueira-ti@outlook.com>
2024-02-06 11:07:35 -0300

    feat: add ToastContainer to handle messages errors and successful

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import { ToastContainer } from 'react-toastify';
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Keirus",
+  description: "Keirus App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>
+        <ToastContainer />
+        {children}
+        </body>
+    </html>
+  );
+}
